44fa194f738a1e0752eab7417f6641028faa1409
piorunj <jordan.piorun@gmail.com>
2016-01-14 18:32:44 +0100

    maj

--- rendu3/src/main/java/fil/iagl/idl/rendu3/App.java
+++ rendu3/src/main/java/fil/iagl/idl/rendu3/App.java
@@ -1,6 +1,10 @@
 package fil.iagl.idl.rendu3;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -40,6 +44,9 @@ public class App {
 	// -c
 	// C:\workspace\IntroClassJava\dataset\smallest\1b31fa5c50f7725ce468ebf24282f2d080a83aed87e4ee35522ae7710c8e0136bc263cc460b8ec7bf2c3519cb59af4a138e114d36541515b2609ab56ad2b8ee9\003\target\classes;C:\workspace\IntroClassJava\dataset\smallest\1b31fa5c50f7725ce468ebf24282f2d080a83aed87e4ee35522ae7710c8e0136bc263cc460b8ec7bf2c3519cb59af4a138e114d36541515b2609ab56ad2b8ee9\003\target\test-classes;C:\Users\wokei\.m2\repository\junit\junit\4.11\junit-4.11.jar
 	// -p C:\workspace\z3-4.3.2-x64-win\bin\z3.exe
+
+	// Resultat initial Resultat [blackSuccess=2284, blackFail=801,
+	// whiteSuccess=2543, whiteFail=1031]
 
 	public static void main(String[] args) throws Exception {
 
@@ -110,7 +117,7 @@ public class App {
 
 		// liste des différents projets
 		Set<File> projets = new HashSet<>();
-		for (File projet : new File("C:/workspace/IntroClassJava/dataset").listFiles(new DirectoryFilter())) {
+		for (File projet : new File("C:/workspace/IntroClassJavaCpy/dataset").listFiles(new DirectoryFilter())) {
 			if (projet.isDirectory())
 				projets.add(projet);
 		}
@@ -123,11 +130,59 @@ public class App {
 							new File(revision.getAbsolutePath() + "/src/main/java/introclassJava").list()[0]);
 					System.out.println("Revision : " + revision);
 					try {
-						MavenRunner.cleanCompileTest(revision + "\\pom.xml");
 						// MavenRunner.clean(revision+"\\pom.xml");
 
 						System.out.println("Path to pom : " + revision + "\\pom.xml");
 						result.put(revision.toString(), new Resultat());
+
+						MavenRunner.cleanCompileTest(revision + "\\pom.xml");
+
+						File testFileName = new File(
+								new File(revision + "/src/test/java/introclassJava").list(new FilenameFilter() {
+
+									@Override
+									public boolean accept(File dir, String name) {
+										return name.contains("White");
+									}
+								})[0]);
+
+						// ProcessBuilder pb = new ProcessBuilder("C:/Program
+						// Files/Java/jdk1.7.0_79/bin/javaw.exe",
+						// "-Dfile.encoding=UTF-8", "-classpath",
+						// "C:/workspace/astor/target/classes;C:/Users/wokei/.m2/repository/fr/inria/gforge/spoon/spoon-core/4.0-SNAPSHOT/spoon-core-4.0-SNAPSHOT.jar;C:/Users/wokei/.m2/repository/org/eclipse/jdt/core/compiler/ecj/4.4/ecj-4.4.jar;C:/Users/wokei/.m2/repository/com/martiansoftware/jsap/2.1/jsap-2.1.jar;C:/Users/wokei/.m2/repository/fr/inria/astor/astor-junit-exec/0.0.2-SNAPSHOT/astor-junit-exec-0.0.2-SNAPSHOT.jar;C:/Users/wokei/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar;C:/Users/wokei/.m2/repository/commons-collections/commons-collections/3.2.1/commons-collections-3.2.1.jar;C:/Users/wokei/.m2/repository/commons-io/commons-io/2.4/commons-io-2.4.jar;C:/Users/wokei/.m2/repository/com/gzoltar/gzoltar/0.1.1/gzoltar-0.1.1.jar;C:/Users/wokei/.m2/repository/junit/junit/4.11/junit-4.11.jar;C:/Users/wokei/.m2/repository/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar;C:/Users/wokei/.m2/repository/commons-cli/commons-cli/1.2/commons-cli-1.2.jar
+						// fr.inria.main.evolution.AstorMain",
+						// "-srcjavafolder", revision + "/src/main/java",
+						// "-srctestfolder",
+						// revision + "/src/test/java", "-binjavafolder
+						// target/classes", "-bintestfolder",
+						// "target/test-classes", "-failing",
+						// "introclassJava." +
+						// testFileName.toString().replace(".java", ""),
+						// "-dependencies",
+						// "examples/Lang-issue-428/lib/junit-4.7.jar",
+						// "-location", "" + revision, "-package",
+						// "introclassJava");
+
+						Runtime rt = Runtime.getRuntime();
+						System.out.println("ICIIIIIII");
+						Process pr = rt.exec(
+								"C:/Program Files/Java/jdk1.7.0_79/bin/javaw.exe -Dfile.encoding=UTF-8 -classpath C:/workspace/astor/target/classes;C:/Users/wokei/.m2/repository/fr/inria/gforge/spoon/spoon-core/4.0-SNAPSHOT/spoon-core-4.0-SNAPSHOT.jar;C:/Users/wokei/.m2/repository/org/eclipse/jdt/core/compiler/ecj/4.4/ecj-4.4.jar;C:/Users/wokei/.m2/repository/com/martiansoftware/jsap/2.1/jsap-2.1.jar;C:/Users/wokei/.m2/repository/fr/inria/astor/astor-junit-exec/0.0.2-SNAPSHOT/astor-junit-exec-0.0.2-SNAPSHOT.jar;C:/Users/wokei/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar;C:/Users/wokei/.m2/repository/commons-collections/commons-collections/3.2.1/commons-collections-3.2.1.jar;C:/Users/wokei/.m2/repository/commons-io/commons-io/2.4/commons-io-2.4.jar;C:/Users/wokei/.m2/repository/com/gzoltar/gzoltar/0.1.1/gzoltar-0.1.1.jar;C:/Users/wokei/.m2/repository/junit/junit/4.11/junit-4.11.jar;C:/Users/wokei/.m2/repository/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar;C:/Users/wokei/.m2/repository/commons-cli/commons-cli/1.2/commons-cli-1.2.jar fr.inria.main.evolution.AstorMain -srcjavafolder C:/workspace/IntroClassJavaCpy/dataset/syllables/2af3c4163b40679ef94bbbbc863c4fa3785c6e164281f5e8850d9e4ffa387e5b162ceb37449ecca33961ff162d861c8d98ff702e723755b6c37c95d5cfaf1c40/003/src/main/java -srctestfolder C:/workspace/IntroClassJavaCpy/dataset/syllables/2af3c4163b40679ef94bbbbc863c4fa3785c6e164281f5e8850d9e4ffa387e5b162ceb37449ecca33961ff162d861c8d98ff702e723755b6c37c95d5cfaf1c40/003/src/test/java -binjavafolder target/classes -bintestfolder target/test-classes -failing introclassJava.syllables_2af3c416_003WhiteboxTest -dependencies examples/Lang-issue-428/lib/junit-4.7.jar -location C:/workspace/IntroClassJavaCpy/dataset/syllables/2af3c4163b40679ef94bbbbc863c4fa3785c6e164281f5e8850d9e4ffa387e5b162ceb37449ecca33961ff162d861c8d98ff702e723755b6c37c95d5cfaf1c40/003 -package introclassJava");
+						System.out.println("ON EST LAAAAAAA");
+						// pr.getInputStream().
+						// Process p = pb.start();
+						// BufferedReader reader = new BufferedReader(new
+						// InputStreamReader(pr.getInputStream()));
+						InputStream is = pr.getInputStream();
+						OutputStream os = new FileOutputStream(new File("blblbl.txt"));
+
+						int read = 0;
+						byte[] bytes = new byte[1024];
+
+						while ((read = is.read(bytes)) != -1) {
+							os.write(bytes, 0, read);
+						}
+						is.close();
+						os.close();
 						XMLParser.readSurefireReport(new File(revision + "/target/surefire-reports"),
 								result.get(revision.toString()));
 						resultGlobal.setBlackFail(
